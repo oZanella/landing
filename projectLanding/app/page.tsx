4e7712c72453163f2e66506afff3c157357707a6
Henrique Zanella <henriquezanella19@gmail.com>
2025-09-16 13:46:28 -0300

    Organização de estrutura

---
+++ projectLanding/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <h1 className="text-3xl font-bold">🚀 Bem-vindo ao meu projeto!</h1>
+      </main>
+    </div>
+  );
+}
