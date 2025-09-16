48763e83be940c776008d05b6f84b6db087714e8
Henrique Zanella <henriquezanella19@gmail.com>
2025-09-16 16:45:34 -0300

    Alteração da estrutura para estudo

--- projectLanding/app/page.tsx
+++ projectLanding/app/page.tsx
@@ -1,9 +1,5 @@
 export default function Home() {
   return (
-    <div>
-      <main>
-        <h1 className="text-3xl font-bold">🚀 Bem-vindo ao meu projeto!</h1>
-      </main>
-    </div>
+    <div />
   );
 }
