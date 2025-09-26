4fea8456f7c209619e4fb66aecda8c1154579641
Henrique Zanella <henriquezanella19@gmail.com>
2025-09-26 16:55:50 -0300

    Inserido todos os dados no footer com links sociais

--- projectLanding/app/pages/Home/header.tsx
+++ projectLanding/app/pages/Home/header.tsx
@@ -2,6 +2,7 @@ import { CardPadrao, CardHeader, CardDescription, CardTitle, CardFooter, CardAct
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui-padrao/avatar-padrao"
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "@/components/ui/themes-toggle";
+import { Facebook, Instagram, Linkedin } from "lucide-react";
 
 
 export default function Header() {
@@ -33,15 +34,14 @@ export default function Header() {
                             <CardTitle className="flex flex-col items-start mb-8 md:gap-2">
                                 Modelo de portfólio!
                             </CardTitle>
-
                             <p className="text-base leading-relaxed break-words">
-                                Um modelo de portfólio limpo, rápido e leve criado com Next.js, Vercel e Tailwind CSS.
+                                Somos uma empresa desenvolvedora de softwares de gestão que desde 2001 vem crescendo e se especializando em ajudar a administrar os negócios dos nossos mais de 8 mil usuários de forma organizada e prática.
                             </p>
                             <p className="text-base leading-relaxed break-words">
-                                O Nextfolio tem tudo o que você precisa para um portfólio: blog MDX, SEO, RSS, feeds Atom e JSON, análises, incorporações de Tweets e YouTube, KaTeX e muito mais.
+                                Nossa gama de produtos se adapta a diferentes ramos de atividades e portes de empresa nos permitindo atender com excelência as micro, pequenas e médias empresas dos segmentos de indústria, comércio e serviços.
                             </p>
                             <p className="text-base leading-relaxed break-words">
-                                O Nextfolio é de código aberto e totalmente personalizável, facilitando a adição de mais recursos.
+                                A otimização dos processos e tarefas diárias é realizada através de um sistema de gestão formado por módulos integrados, utilizados conforme a necessidade do cliente.
                             </p>
                         </div>
                         <div className="flex-shrink-0 mx-auto md:mx-0 md:ml-6">
@@ -51,18 +51,31 @@ export default function Header() {
                             </Avatar>
                         </div>
                     </div>
-
                     <div className="flex flex-col text-start gap-2">
                         <p className="leading-relaxed break-words">
-                            Implante seu site Nextfolio com o Vercel em minutos e siga as instruções de configuração na postagem Introdução.                        </p>
+                            Desde sua abertura, a empresa vem aumentando sua equipe, que conta hoje com 13 unidades de negócios e mais de 100 profissionais atualizados constantemente na velocidade exigida pelo mercado, proporcionando a seus clientes, produtos e serviços com excelência e confiabilidade.                      </p>
                         <p className="leading-relaxed break-words">
-                            Construído e mantido pela Sirius.
+                            Juntos somos mais fortes.
                         </p>
                     </div>
                 </div>
             </CardContent>
-
-
+            <CardFooter>
+                <div className="flex flex-row justify-between items-center w-full mt-6 text-sm text-[#1C1C1C] dark:text-[#D4D4D4]">
+                    <p>© 2025 Windel Web</p>
+                    <div className="flex gap-4 text-[#1C1C1C] dark:text-[#D4D4D4]">
+                        <a href="https://www.instagram.com/windelsistemas/#" target="_blank" rel="noopener noreferrer">
+                            <Instagram className="cursor-pointer hover:scale-110 transition" />
+                        </a>
+                        <a href="https://www.facebook.com/windelsistemas" target="_blank" rel="noopener noreferrer">
+                            <Facebook className="cursor-pointer hover:scale-110 transition" />
+                        </a>
+                        <a href="https://www.linkedin.com/company/windel-sistemas/" target="_blank" rel="noopener noreferrer">
+                            <Linkedin className="cursor-pointer hover:scale-110 transition" />
+                        </a>
+                    </div>
+                </div>
+            </CardFooter>
 
         </CardPadrao>
     );
