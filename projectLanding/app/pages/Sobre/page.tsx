cd31b2237290702331a18ff47c71da93b53723e7
Henrique Zanella <henriquezanella19@gmail.com>
2025-10-02 16:46:41 -0300

    Inserido modelo de card na aba sobre

--- projectLanding/app/pages/Sobre/page.tsx
+++ projectLanding/app/pages/Sobre/page.tsx
@@ -1,31 +1,66 @@
-import { CardTitle, CardContent } from "@/components/ui-padrao/card-padrao";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui-padrao/avatar-padrao"
 
+import { CardPadraoMini, CardContent, CardTitle } from "@/components/ui-padrao/card-padrao-mini";
 import { Separator } from "@/components/ui/separator";
+import Link from "next/link";
+
 
 
 export default function PageIndicacao() {
     return (
 
         <CardContent className="gap-4">
-            <div className="flex flex-col gap-2 md:max-h-[65vh] md:overflow-auto scrollbar-thin">
-                <div className="flex flex-col-reverse items-center gap-2 md:flex-row md:items-start">
-                    <div className="flex-1 min-w-0 flex flex-col text-start gap-3 mb-2 md:text-left">
-                        <CardTitle className="flex flex-col md:items-start mb-0 justify-center items-center md:gap-2">
-                            Refêrencias do Projeto
-                        </CardTitle>
-                        <Separator className="my-4" />
+            <CardPadraoMini>
+                <CardContent>
+                    <div className="flex flex-col gap-2 md:max-h-[65vh] md:overflow-auto scrollbar-thin">
+                        <div className="flex flex-col-reverse items-center gap-2 md:flex-row md:items-start">
+                            <div className="flex-1 min-w-0 flex flex-col text-start gap-1 mb-2 md:text-left">
+                                <CardTitle className="flex flex-col md:items-start mb-0 justify-center items-center md:gap-2">
+                                    Desenvolvedor do Projeto
+                                </CardTitle>
+                                <Separator className="my-4" />
+                                <p className="font-bold">Todo o conteúdo, incluindo informações e imagens, foi obtido a partir do site oficial e utilizado para fins de referência.</p>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div className="flex flex-col-reverse items-center gap-2 md:flex-row md:items-start">
-                    <div className="flex-1 min-w-0 flex flex-col text-start gap-3 mb-2 md:text-left">
-                        <CardTitle className="flex flex-col md:items-start mb-0 justify-center items-center md:gap-2">
-                            Conheça o Desenvolvedor
-                        </CardTitle>
-                        <Separator className="my-4" />
+                </CardContent>
+            </CardPadraoMini>
+
+            <Separator className="my-4" />
+
+            <CardPadraoMini>
+                <CardContent>
+                    <div className="flex flex-col gap-2 md:max-h-[65vh] md:overflow-auto scrollbar-thin">
+                        <div className="flex flex-col-reverse items-center gap-2 md:flex-row md:items-start">
+                            <div className="flex-1 min-w-0 flex flex-col text-start gap-1 mb-2 md:text-left">
+                                <CardTitle className="flex flex-col md:items-start mb-0 justify-center items-center md:gap-2">
+                                    Linguagens utilizadas no Projeto
+                                </CardTitle>
+                                <Separator className="my-4" />
+                                <p className="font-bold">Todo o conteúdo, incluindo informações e imagens, foi obtido a partir do site oficial e utilizado para fins de referência.</p>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-        </CardContent>
+                </CardContent>
+            </CardPadraoMini>
+
+            <Separator className="my-4" />
+
+            <CardPadraoMini>
+                <CardContent>
+                    <div className="flex flex-col gap-2 md:max-h-[65vh] md:overflow-auto scrollbar-thin">
+                        <div className="flex flex-col-reverse items-center gap-2 md:flex-row md:items-start">
+                            <div className="flex-1 min-w-0 flex flex-col text-start gap-1 mb-2 md:text-left">
+                                <CardTitle className="flex flex-col md:items-start mb-0 justify-center items-center md:gap-2">
+                                    Refêrencias do Projeto
+                                </CardTitle>
+                                <Separator className="my-4" />
+                                <p className="font-bold">Conteúdo e imagens fornecidos pelo site oficial, utilizados apenas como referência.</p>
+                                <Link href={'https://www.windel.com.br/'}>https://www.windel.com.br</Link>
+                            </div>
+                        </div>
+                    </div>
+                </CardContent>
+            </CardPadraoMini>
+        </CardContent >
     );
 }
