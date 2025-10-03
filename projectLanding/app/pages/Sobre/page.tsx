e6d5383559ebb4ee24924470a63b07ccf5922a9c
Henrique Zanella <henriquezanella19@gmail.com>
2025-10-03 15:41:02 -0300

    Alterado ícones para icons com colors

--- projectLanding/app/pages/Sobre/page.tsx
+++ projectLanding/app/pages/Sobre/page.tsx
@@ -7,7 +7,6 @@ import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { AvatarImage } from "@radix-ui/react-avatar";
 import { useTheme } from "next-themes";
 
-
 export default function PageIndicacao() {
 
     const { theme } = useTheme();
@@ -19,15 +18,20 @@ export default function PageIndicacao() {
             logoDark: '/images/nextjsThemeLight.svg'
         },
         {
-            nome: 'Tailwind CSS',
-            logoLight: '/images/tailwindThemeDark.svg',
-            logoDark: '/images/tailwindThemeLight.svg'
-
+            nome: 'React',
+            logoLight: '/images/react.svg',
+            logoDark: '/images/react.svg'
         },
         {
             nome: 'TypeScript',
-            logoLight: '/images/typescriptThemeDark.svg',
-            logoDark: '/images/typescriptThemeLight.svg'
+            logoLight: '/images/typescript.svg',
+            logoDark: '/images/typescript.svg'
+        },
+        {
+            nome: 'Tailwind CSS',
+            logoLight: '/images/tailwindcss.svg',
+            logoDark: '/images/tailwindcss.svg'
+
         }
     ]
 
@@ -76,14 +80,24 @@ export default function PageIndicacao() {
 
                         <CardPadraoMini>
                             <CardContent>
-                                <div className="flex flex-col gap-2 md:max-h-[65vh] md:overflow-auto scrollbar-thin">
-                                    <div className="flex flex-col-reverse items-center gap-2 md:flex-row md:items-start">
-                                        <div className="flex-1 min-w-0 flex flex-col text-start gap-1 mb-2 md:text-left">
-                                            <CardTitle className="flex flex-col md:items-start mb-0 justify-center items-center md:gap-2">
-                                                Desenvolvedor do Projeto
+                                <div className="flex flex-col md:max-h-[65vh] md:overflow-auto scrollbar-thin">
+                                    <div className="flex items-center md:flex-row md:items-start">
+                                        <div className="flex-1 flex flex-col text-start md:text-left mt-4 md:mt-0">
+                                            <CardTitle className="flex flex-col justify-center items-center md:items-start md:justify-center md:gap-2">
+                                                Arquitetura do Sistema
                                             </CardTitle>
+
                                             <Separator className="my-4" />
-                                            <p className="font-bold">Todo o conteúdo, incluindo informações e imagens, foi obtido a partir do site oficial e utilizado para fins de referência.</p>
+
+                                            <CardPadraoMini>
+                                                <div className="flex flex-col md:flex-row gap-10 md:gap-2 m-2 p-2 md:p-4 items-center justify-between">
+                                                    <div className="flex flex-col items-center justify-center">
+
+
+                                                    </div>
+                                                </div>
+                                            </CardPadraoMini>
+
                                         </div>
                                     </div>
                                 </div>
